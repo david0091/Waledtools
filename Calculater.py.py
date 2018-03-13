9952391587a30f74069aff3c0b013a67526522d6
david0091 <36229896+david0091@users.noreply.github.com>
2018-03-13 13:35:00 +0100

    Add files via upload

---
+++ Calculater.py.py
@@ -0,0 +1,41 @@
+class calcu:
+    def add():
+        x = int(input ("Enter a Number :: "))
+        y = int(input("Enter a Number ;: "))
+        c = x + y
+        print("is " ,c)
+
+    def suptra():
+        x = input ("Enter a Number :: ")
+        y = input("Enter a Number ;: ")
+        c = x - y
+        print("is " ,c)
+        
+    def multi():
+        x = int(input ("Enter a Number :: "))
+        y = int(input("Enter a Number ;: "))
+        c = x  * y
+        print("is " ,c)
+
+    def divi():
+        x = int(input ("Enter a Number :: "))
+        y = int(input("Enter a Number ;: "))
+        c = x / y
+        print("is " ,c)
+
+   
+def cal():
+    x = str(input("Enter a option "))
+    if x == '+':
+        calcu.add()
+    elif x == '-':
+        calcu.suptra()
+    elif x =='*':
+        calcu.multi()
+    elif x =='/':
+        calcu.divi()
+    else:
+        print("False")
+
+cal()
+
